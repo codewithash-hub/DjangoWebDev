629fd4e9be9f38e75849a7764a5dbb76a0e2c807
codewithash <402308182@my.richfield.ac.za>
2025-05-17 00:13:31 +0200

    Defining a default sort order and Adding a database index to  improve performance for query filtering or ordering results by this field

--- Django5ByExample/BlogApp/blog/models.py
+++ Django5ByExample/BlogApp/blog/models.py
@@ -3,14 +3,21 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-  title = models.CharField(max_length=250)
-  slug = models.SlugField(max_length=250)
-  body = models.TextField()
-  publish = models.DateTimeField(default=timezone.now)
-  created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now_add=True)
-  
-  def __str__(self):
-      return self.title
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    #  Defining a default sort order
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+    
+    def __str__(self):
+        return self.title
   
   
