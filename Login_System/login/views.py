c2fe7fb9c05b6658edfe78587fa7846996422b1e
codewithash <402308182@my.richfield.ac.za>
2025-07-13 12:44:05 +0200

    creating views for signup and signin

--- Login_System/login/views.py
+++ Login_System/login/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render;
+from django.http import HttpResponse;
 
 # Create your views here.
+def home(request):
+    return render(request, "login/index.html")
+
+def signup(request):
+    return render(request, "login/signup.html")
+
+def signin(request):
+    return render(request, "login/signin.html")
+
+def signout(request):
+    pass
